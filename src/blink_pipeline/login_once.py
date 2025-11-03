5657b4471f186392bfb6f5c2d2990a463cfc487c
denver <cudedenver@gmail.com>
2025-11-02 16:46:21 -0800

    fix: BlinkTwoFARequiredError improted from wrong library file

--- src/blink_pipeline/login_once.py
+++ src/blink_pipeline/login_once.py
@@ -7,7 +7,7 @@ from aiohttp import ClientSession
 from blinkpy.blinkpy import Blink
 from blinkpy.auth import Auth
 from blinkpy.helpers.util import json_load
-from blinkpy.exceptions import BlinkTwoFARequiredError
+from blinkpy.auth import BlinkTwoFARequiredError
 
 load_dotenv()
 AUTH_PATH = Path(os.getenv("BLINK_AUTH_PATH", "blink_auth.json"))
